b13b455992ed151fa4de98a6ef4fa86303b27190
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-08 16:36:29 -0300

    updating arrays

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-// update objects
+const numbers = [1, 2, 3];
 
-const person = {
-  name: 'Ed', address: {
-    country: 'United States',
-    city: 'San Francisco',
-  }
-};
-const updated = {
-  ...person,
-  address: {
-    ...person.address,
-    city: 'New York',
-  },
-  name: 'Eddy'
-};
-console.log('person', updated)
+// adding
+const index = numbers.indexOf(2);
+const added = [
+  ...numbers.slice(0, index),
+  4,
+  ...numbers.slice(index)
+];
+
+console.log('numbers', added);
+
+// removing
+
+const removed = numbers.filter(n => n !== 2);
+
+console.log('removed', removed);
+
+// updating
+const updated = numbers.map(n => n === 2 ? 20 : n);
+console.log('updated', updated);
