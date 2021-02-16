5f230b6743d617abee3e58b5bcbbf9ed59bbb4b4
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-16 17:46:05 -0300

    dispatching functions

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import configureStore from './store/configureStore';
-import {
-  bugAdded,
-  bugResolved,
-  bugAssignedToUser,
-  getUnresolvedBugs,
-  getBugsByUser
-} from './store/bugs';
-import { projectAdded } from './store/projects';
-import { usersAdded } from './store/users';
 
 const store = configureStore();
 
-store.dispatch(usersAdded({ name: 'Edilson' }));
-/* store.dispatch(usersAdded({ name: 'Moisés' }));
-store.dispatch(projectAdded({ name: 'Project 1' }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugResolved({ id: 1 })); */
-
-//exercise - get bug user
-// add bug - user
-/* store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-//get bug - user
-const bugs = getBugsByUser(1)(store.getState()); */
-
+store.dispatch((dispatch, getState) => {
+  //Call an Api
+  // When the promise is resolved => dispatch()
+  dispatch({ type: 'bugsReceived', bugs: [1, 2, 3] });
+  console.log(getState())
+  // If the promise is rejected => dispatch()
+});
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import reducer from './reducer';
 import logger from './middleware/logger'
 
 
+
 export default () => configureStore({
   reducer,
-  middleware: [logger({ destination: "console" })]
+  middleware: [
+    ...getDefaultMiddleware(),
+    logger({ destination: "console" }),
+  ]
 });
