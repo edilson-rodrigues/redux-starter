f8a4201cb4ee0ea90ce37040b7d1511c70bd1074
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-16 14:37:54 -0300

    create store clean

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,4 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit'
 import reducer from './bugs';
 
-export default function configureStore() {
-  const store = createStore(
-    reducer,
-    devToolsEnhancer({ trace: true })
-  );
-  return store;
-};
+export default () => configureStore({ reducer });
