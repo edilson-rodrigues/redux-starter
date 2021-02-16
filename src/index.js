683d51ed3f674161466a80f1757e4e8d4e3aac64
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-16 12:35:42 -0300

    importing ad importing state redux dev tools

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(actions.bugAdded("Bug 1"));
+/* store.dispatch(actions.bugAdded("Bug 1"));
 store.dispatch(actions.bugAdded("Bug 2"));
 store.dispatch(actions.bugAdded("Bug 3"));
-store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugResolved(1)); */
 
 console.log('getstate', store.getState());
 
