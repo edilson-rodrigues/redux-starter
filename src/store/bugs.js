071d1e43bad37ce101606f8cb6bda5f1c19b88a2
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-16 16:18:34 -0300

    memoizing selectors whi reselect

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createSelector } from 'reselect';
 
 let lastId = 0;
 
@@ -24,11 +25,17 @@ export const { bugAdded, bugResolved } = slice.actions;
 
 export default slice.reducer;
 
-// Selectors
+/* // Selectors
 export const getUnresolvedBugs = state =>
-  state.entities.bugs.filter(bug => !bug.resolved);
+  state.entities.bugs.filter(bug => !bug.resolved); */
 
-
+// Memoization
+// bugs => get unresolved bugs from cache
+export const getUnresolvedBugs = createSelector(
+  state => state.entities.bugs,
+  state => state.entities.projects,
+  (bugs, projects) => bugs.filter(bug => !bug.resolved)
+)
 
 
 
