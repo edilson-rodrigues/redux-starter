5b10bfc2ceb14cb79ce0845d0aeb800e0eea6f29
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-16 15:37:48 -0300

    exercise clean redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import reducer from './bugs';
+import reducer from './projects';
+/* import reducer from './bugs'; */
 
 export default () => configureStore({ reducer });
