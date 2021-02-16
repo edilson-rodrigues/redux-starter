eabfdb2d6846e778b33736c7ada6de580e944864
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-16 11:58:21 -0300

    subscribing to the store

--- src/customStore.js
+++ src/customStore.js
@@ -2,17 +2,28 @@ import reducer from './store/reducer/reducer';
 
 function createStore(reducer) {
   let state;
+  let listeners = [];
 
   function getState() {
     return state;
   }
 
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
+
   function dispatch(action) {
     // call the reducer to get the new state
     state = reducer(state, action);
+
+    // listener
+    for (let i = 0; i < listeners.length; i++) {
+      listeners[i]();
+    }
   }
 
   return {
+    subscribe,
     dispatch,
     getState
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import store from './customStore';
 import * as actions from './store/actions/actions';
 
+store.subscribe(() => {
+  console.log("Store changed!");
+});
 
 store.dispatch(actions.bugAdded("Bug 1"));
 
