047635bc8cccfb135118d2e80dc91c4ed1a8b5ff
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-22 16:03:06 -0300

    solution - get unresolvedbugs

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -111,7 +111,7 @@ export const getUnresolvedBugs = state =>
 export const getUnresolvedBugs = createSelector(
   state => state.entities.bugs,
   state => state.entities.projects,
-  (bugs, projects) => bugs.filter(bug => !bug.resolved)
+  (bugs, projects) => bugs.list.filter(bug => !bug.resolved)
 )
 
 export const getBugsByUser = userId =>
