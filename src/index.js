55d024d159f4ebaefa6cd22eb9cb708311f8a824
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-22 10:29:01 -0300

    solution resolvig bugs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import configureStore from './store/configureStore';
-import { addBug } from './store/bugs';
+import { loadBugs, resolveBug } from './store/bugs';
 
 const store = configureStore();
 
 //UI layer
-store.dispatch(addBug({
-  description: 'a'
-}));
+store.dispatch(loadBugs());
+
+setTimeout(() => store.dispatch(resolveBug(1)), 2000);
 
 
 
